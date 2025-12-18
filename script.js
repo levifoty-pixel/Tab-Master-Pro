715ad2667d2cda906d69dabd02bea67118abc965
levifoty-pixel <levifoty@gmail.com>
2025-12-18 13:48:10 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simple view switching
+const navItems = document.querySelectorAll('.nav-item');
+const views = document.querySelectorAll('.view');
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const target = item.getAttribute('data-view');
+
+    views.forEach(v => v.classList.add('hidden'));
+    document.getElementById(`view-${target}`).classList.remove('hidden');
+  });
+});
